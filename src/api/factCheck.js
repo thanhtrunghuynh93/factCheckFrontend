0c6390b4292f2fb598a011a040e9122d242a906f
huongvi-nguyen <thihuongvi-nguyen@dac.co.jp>
2024-04-05 21:27:10 +0900

    update flow

---
+++ src/api/factCheck.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+	baseURL: `${process.env.REACT_APP_API_URL}/factCheckNew`,
+	headers: {
+		accept: 'application/json',
+	},
+});
+
+export async function getLink({claim}) {
+    try {
+        const res = await axiosInstance.post('get_links', {
+            claim
+        })
+        return {
+            success: true,
+            data: res.data.data
+        }
+    } catch (error) {
+        return {
+            success: false
+        }
+    }
+}
+
+
+export async function verifySingleLink({claim, url}) {
+    try {
+        const res = await axiosInstance.post('verify_single_link', {
+            claim,
+            url
+        })
+        return {
+            success: true,
+            data: res.data.data
+        }
+    } catch (error) {
+        return {
+            success: false
+        }
+    }
+}
+
+
+export async function summarise({claim, item}) {
+    try {
+        const res = await axiosInstance.post('summarise_evidence', {
+            claim,
+            item
+        })
+        return {
+            success: true,
+            data: res.data.data
+        }
+    } catch (error) {
+        return {
+            success: false
+        }
+    }
+}
