dfdb9fc8855f6fcee0f4b0f0da1c7ecddb0669e9
vuviethuung1998 <vuviethung.98.hust@gmail.com>
2024-04-07 14:31:28 +0700

    update

--- src/api/factCheck.js
+++ src/api/factCheck.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: `${process.env.REACT_APP_API_URL}/factCheckNew`,
+	baseURL: `https://api.lsirfactcheck.org/factCheckNew`,
 	headers: {
 		accept: 'application/json',
 	},
